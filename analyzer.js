9b5a3960e14a04867f71a17538fa59d300498bf4
Ram Kalluri <rmkalluri@gmail.com>
2026-04-11 17:35:13 -0700

    Parse primary and secondary keys.

--- analyzer.js
+++ analyzer.js
@@ -2,12 +2,44 @@ let timer;
 
 // SDP = Session Description Protocol, Defined in RFC 8866
 
+const NewLineRegex = /\r?\n/;
+const DescriptionLineRegex = /([^=]*)=(.*)/;
+const PrimaryKeys = "vostm";
+const SecondaryKeys = {
+    null: "",
+    "m": "ac",
+    "t": "ar",
+}
+
 function parseSDP() {
     let sdp = document.getElementById("sdp-input").value;
     console.log("parsing SDP");
-    let lines = sdp.split(/\r?\n/);
-    for (const line of lines) {
-        console.log(line)
+
+    let sections = [];
+    let currSectionKey = null;
+    for (const [index, line] of sdp.split(NewLineRegex).entries()) {
+        if (line.length == 0) {
+            console.log("line ${index} empty");
+            continue;
+        }
+
+        let [, key, value] = line.match(DescriptionLineRegex);
+        if (PrimaryKeys.includes(key)) {
+            // Create new section and save the section key
+            sections.push([ [index, key, value] ]);
+            currSectionKey = key;
+        } else if (SecondaryKeys[currSectionKey]?.includes(key)) {
+            // Append to the last section
+            sections.at(-1).push([index, key, value]);
+        } else {
+            // An unexpected key was found. Do we need to update
+            // the primary or secondary keys?
+            console.log("discarding line ${index}, ${line}");
+        }
+    }
+
+    for (const s of sections) {
+        console.log(s)
     }
 }
 
