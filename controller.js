e14fac975e2176da7aebea54aaf29991b056da1f
Ram Kalluri <rmkalluri@gmail.com>
2026-04-13 05:14:54 -0700

    Organize into MVC modules.

---
+++ controller.js
@@ -0,0 +1,47 @@
+
+let lineHeight;
+let sdpInputTimer;
+let displayedLine;
+let sdpDescriptions = {};
+
+function onSDPInput() {
+    // clear previous timer
+    clearTimeout(sdpInputTimer);
+    // start new timer
+    sdpInputTimer = setTimeout(() => { sdpDescriptions = parseSDP(); }, 700);
+}
+
+function onSDPLineClick(e) {
+    let sdpInput = document.getElementById("sdp-input");
+    if (sdpInput.value.length == 0) {
+        return;
+    }
+
+    const rect = sdpInput.getBoundingClientRect();
+    let linepos = Math.floor((e.clientY - rect.top) / lineHeight);
+    if (displayedLine != linepos) {
+        displayedLine = linepos;
+        let description = mapLineToDescription(sdpDescriptions, linepos);
+        console.log(`Description = ${description}`);
+    }
+}
+
+window.addEventListener('load', () => {
+    // This function calculates the height of each line
+    // in the textarea in a browser-independent way.
+    let element = document.getElementById("sdp-input");
+    const clone = element.cloneNode(false);
+    clone.innerHTML = "A"; // Use a single character
+    clone.style.padding = "0";
+    clone.style.border = "0";
+    clone.style.visibility = "hidden";
+    clone.style.position = "absolute";
+    clone.style.minHeight = "1lh"
+  
+    document.body.appendChild(clone);
+    lineHeight = clone.offsetHeight; // This is the line height in pixels
+    document.body.removeChild(clone);
+});
+
+window.onSDPInput = onSDPInput;
+window.onSDPLineClick = onSDPLineClick;
--- model.js
+++ model.js
@@ -5,11 +5,6 @@
 const NewLineRegex = /\r?\n/;
 const DescriptionLineRegex = /([^=]*)=(.*)/;
 
-let lineHeight;
-let sdpInputTimer;
-let sdpDescriptions = {};
-let displayedLine;
-
 class Description {
     constructor(lines) {
         this.lines = structuredClone(lines);
@@ -32,7 +27,6 @@ function parseSDP() {
     }
 
     let sections = [];
-    let currSectionKey = null;
     for (const [index, line] of sdp.split(NewLineRegex).entries()) {
         if (line.length == 0) {
             console.debug(`line ${index} empty`);
@@ -47,13 +41,15 @@ function parseSDP() {
         }
     }
 
-    sdpDescriptions = [];
+    let descriptions = [];
     for (const s of sections) {
         let d = new Description(s);
-        sdpDescriptions[d.firstLine()] = d;
+        descriptions[d.firstLine()] = d;
     }
 
     console.debug("Done parsing SDP");
+
+    return descriptions;
 }
 
 function mapLineToDescription(line) {
@@ -68,45 +64,3 @@ function mapLineToDescription(line) {
     }
     return prevDescription;
 }
-
-function onSDPInput() {
-    // clear previous timer
-    clearTimeout(sdpInputTimer);
-    // start new timer
-    sdpInputTimer = setTimeout(() => { parseSDP(); }, 700);
-}
-
-function onSDPLineClick(e) {
-    let sdpInput = document.getElementById("sdp-input");
-    if (sdpInput.value.length == 0) {
-        return;
-    }
-
-    const rect = sdpInput.getBoundingClientRect();
-    let linepos = Math.floor((e.clientY - rect.top) / lineHeight);
-    if (displayedLine != linepos) {
-        displayedLine = linepos;
-        let description = mapLineToDescription(linepos);
-        console.log(`Description = ${description}`);
-    }
-}
-
-window.addEventListener('load', () => {
-    // This function calculates the height of each line
-    // in the textarea in a browser-independent way.
-    let element = document.getElementById("sdp-input");
-    const clone = element.cloneNode(false);
-    clone.innerHTML = "A"; // Use a single character
-    clone.style.padding = "0";
-    clone.style.border = "0";
-    clone.style.visibility = "hidden";
-    clone.style.position = "absolute";
-    clone.style.minHeight = "1lh"
-  
-    document.body.appendChild(clone);
-    lineHeight = clone.offsetHeight; // This is the line height in pixels
-    document.body.removeChild(clone);
-});
-
-window.onSDPInput = onSDPInput;
-window.onSDPLineClick = onSDPLineClick;
